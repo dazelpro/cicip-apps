857ab86b60ba817fa732ac16f4266dc23b5b0f6d
dazelpro <dazelpro@gmail.com>
2021-05-02 18:10:05 +0700

    clone template

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/small.css';
+import '../styles/medium.css';
+import '../styles/large.css';
+import '../styles/xlarge.css';
 
 console.log('Hello Coders! :)');
