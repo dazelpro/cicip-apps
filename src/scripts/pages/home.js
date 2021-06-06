b7f95f592bf3ad56f3f046794e01b7fd72cb42d7
dazelpro <dazelpro@gmail.com>
2021-06-06 21:15:53 +0700

    image lazy load

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -34,7 +34,7 @@ const Home = {
             resto.restaurants.forEach((d) => {
                 dataList += `
                 <a href="/#/detail/${d.id}" class="card">
-                    <img src="${CONFIG.BASE_IMAGE_URL_SMALL + d.pictureId}" alt="${d.name}" loading="lazy">
+                    <img loading="lazy" src="${CONFIG.BASE_IMAGE_URL_SMALL + d.pictureId}" alt="${d.name}">
                     <div class="card-body">
                         <div class="city">Kota ${d.city}</div>
                         <div class="name">${d.name}</div>
